6b4caac093ce5683707c021a1947033d7a8cec8b
harathi <harathinenavath@gmail.com>
2024-05-01 11:35:04 +0530

    changes made

---
+++ calsi.js
@@ -0,0 +1,13 @@
+var aud1=new Audio();
+aud1.src="/placement_activity/asset/adiuo/pick.mp3"
+
+function tap(){
+    aud1.play();
+}
+var aud2=new Audio();
+aud2.src="/placement_activity/asset/adiuo/click-button.mp3"
+
+function touch(){
+    aud2.play();
+}
+// tap()
